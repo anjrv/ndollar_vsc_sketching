9a79cfb2bc6af83332121f429992f700a3769362
Anna Jaerving <AnnaJrving@gmail.com>
2023-07-13 18:03:38 +0000

    move over fix

--- sketching-vscode-extension/src/extension.ts
+++ sketching-vscode-extension/src/extension.ts
@@ -44,23 +44,23 @@ export function activate(context: vscode.ExtensionContext) {
           .map(Number)
       );
 
-      console.log(
-        await sketchingClient.parseSketch(
-          new ParseSketchRequest().setStrokesList([
-            new Stroke().setPointsList([
-              new Point().setX(cordinates[0]![0]).setY(cordinates[0]![1]),
-              new Point().setX(cordinates[1]![0]).setY(cordinates[1]![1]),
-              new Point().setX(cordinates[2]![0]).setY(cordinates[2]![1]),
-            ]),
-          ])
-        )
-      );
-
       if (
         cordinates[0]?.length === 2 &&
         cordinates[1]?.length === 2 &&
         cordinates[2]?.length === 2
       ) {
+        console.log(
+          await sketchingClient.parseSketch(
+            new ParseSketchRequest().setStrokesList([
+              new Stroke().setPointsList([
+                new Point().setX(cordinates[0]![0]).setY(cordinates[0]![1]),
+                new Point().setX(cordinates[1]![0]).setY(cordinates[1]![1]),
+                new Point().setX(cordinates[2]![0]).setY(cordinates[2]![1]),
+              ]),
+            ])
+          )
+        );
+
         const pos1 = new vscode.Position(cordinates[0][0], cordinates[0][1]);
         const pos2 = new vscode.Position(cordinates[1][0], cordinates[1][1]);
         let movePos = new vscode.Position(cordinates[2][0], cordinates[2][1]);
@@ -72,7 +72,7 @@ export function activate(context: vscode.ExtensionContext) {
           const emptyLinesRequired = lineCount < requiredlineCount;
           const edit = new vscode.WorkspaceEdit();
 
-          //replace text
+          //replace selected text for ""
           for (
             let currentLine = pos1.line;
             currentLine <= pos2.line;
@@ -90,10 +90,9 @@ export function activate(context: vscode.ExtensionContext) {
               new vscode.Range(currentPos1, currentPos2)
             );
             lines.push(line);
-            edit.replace(
+            edit.delete(
               editor.document.uri,
-              new vscode.Range(currentPos1, currentPos2),
-              ''
+              new vscode.Range(currentPos1, currentPos2)
             );
           }
 
@@ -101,6 +100,7 @@ export function activate(context: vscode.ExtensionContext) {
           //text exceeds linecount and is the bottom text
           if (emptyLinesRequired && movePos.line >= lineCount) {
             let counter = 0;
+
             for (
               let currentLine = lineCount;
               currentLine <= requiredlineCount;
@@ -123,7 +123,6 @@ export function activate(context: vscode.ExtensionContext) {
               }
             }
           }
-
           //text exceeds linecount but is not bottom text
           else if (emptyLinesRequired && movePos.line < lineCount) {
             //remove all lines at the bottom so we can relocate them
@@ -134,7 +133,7 @@ export function activate(context: vscode.ExtensionContext) {
               currentLine--
             ) {
               const currentPos1 = new vscode.Position(currentLine, 0);
-              const currentPos2 = new vscode.Position(currentLine, 100);
+              const currentPos2 = new vscode.Position(currentLine + 1, 0);
               const line = editor.document.getText(
                 new vscode.Range(currentPos1, currentPos2)
               );
@@ -144,34 +143,32 @@ export function activate(context: vscode.ExtensionContext) {
                 new vscode.Range(currentPos1, currentPos2)
               );
             }
-
-            //add new lines
-            let bigString = '';
+            //Insert selected lines into new position
+            let selectedLines = '';
             for (let i = 0; i < lines.length; i++) {
-              bigString += lines[i] + '\n';
-            }
-            edit.insert(
-              editor.document.uri,
-              new vscode.Position(movePos.line, 0),
-              bigString
-            );
-            let bottomBigString = '';
-            for (let i = bottomLines.length - 1; i >= 0; i--) {
-              if (i !== 0) {
-                bottomBigString += bottomLines[i] + '\n';
+              if (i !== lines.length - 1) {
+                selectedLines += lines[i] + '\n';
               } else {
-                bottomBigString += bottomLines[i];
+                selectedLines += lines[i];
               }
             }
             edit.insert(
               editor.document.uri,
               new vscode.Position(movePos.line, 0),
-              bottomBigString
+              selectedLines
+            );
+            let bottomLinesString = '';
+            for (let i = bottomLines.length - 1; i >= 0; i--) {
+              bottomLinesString += bottomLines[i];
+            }
+            edit.insert(
+              editor.document.uri,
+              new vscode.Position(movePos.line, 0),
+              bottomLinesString
             );
           }
 
           //text does not exceed the linecount
-          //TODO: Make it so that text does not overlap with other text
           else {
             for (let i = 0; i < lines.length; i++) {
               const line = lines[i];
@@ -182,13 +179,38 @@ export function activate(context: vscode.ExtensionContext) {
               );
             }
           }
+
           await vscode.workspace.applyEdit(edit);
           vscode.window.showInformationMessage('Text successfully moved!');
         } else {
           vscode.window.showErrorMessage('No active editor found!');
         }
       } else {
-        vscode.window.showErrorMessage('Inputs failed');
+        vscode.window.showErrorMessage('Inputs failed!');
+      }
+    })
+  );
+
+  // command that deletes all lines.
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('move_text.remove_all_lines', async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const lines = editor.document.lineCount;
+        let edit = new vscode.WorkspaceEdit();
+
+        for (let i = lines - 1; i >= 0; i--) {
+          const currentPos1 = new vscode.Position(i, 0);
+          const currentPos2 = new vscode.Position(i + 1, 0);
+          const range = new vscode.Range(currentPos1, currentPos2);
+          edit.delete(editor.document.uri, range);
+        }
+
+        await vscode.workspace.applyEdit(edit);
+        vscode.window.showInformationMessage('Lines succesfully deleted!');
+      } else {
+        vscode.window.showErrorMessage('No active editor found!');
       }
     })
   );
